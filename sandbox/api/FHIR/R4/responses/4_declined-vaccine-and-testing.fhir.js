7b33bf5dbd61664e97c6ed8988f4bd6fbbf822d6
Joel Klinger <joel.klinger1@nhs.net>
2022-02-25 15:24:01 +0000

    [DALA 777] remove authored from responses

--- sandbox/api/FHIR/R4/responses/4_declined-vaccine-and-testing.fhir.js
+++ sandbox/api/FHIR/R4/responses/4_declined-vaccine-and-testing.fhir.js
@@ -1,61 +1,63 @@
 export default (id) => ({
-    "resourceType": "Bundle",
-    "type": "searchset",
-    "entry": [
-        {
-            "fullUrl": `QuestionnaireResponse/${id}`,
-            "resource": {
-                "resourceType": "QuestionnaireResponse",
-                "id": "2fa8f1b8-caea-4f3d-9978-c0839da568b2",
-                "questionnaire": "https://fhir.nhs.uk/Questionnaire/COVIDVaccinationMedicalExemption",
-                "status": "completed",
-                "subject": {
-                    "reference": "#p1",
-                    "identifier": {
-                        "system": "https://fhir.nhs.uk/Id/nhs-number",
-                        "value": id
-                    },
-                    "display": "John Jonah Jameson"
+  resourceType: "Bundle",
+  type: "searchset",
+  entry: [
+    {
+      fullUrl: `QuestionnaireResponse/${id}`,
+      resource: {
+        resourceType: "QuestionnaireResponse",
+        id: "2fa8f1b8-caea-4f3d-9978-c0839da568b2",
+        questionnaire:
+          "https://fhir.nhs.uk/Questionnaire/COVIDVaccinationMedicalExemption",
+        status: "completed",
+        subject: {
+          reference: "#p1",
+          identifier: {
+            system: "https://fhir.nhs.uk/Id/nhs-number",
+            value: id,
+          },
+          display: "John Jonah Jameson",
+        },
+        contained: [
+          {
+            id: "p1",
+            resourceType: "Patient",
+            birthDate: "1960-01-01",
+          },
+        ],
+        item: [
+          {
+            linkId: "exemptionStatus",
+            answer: [
+              {
+                valueCoding: {
+                  system:
+                    "https://fhir.nhs.uk/CodeSystem/covid-vaccination-medical-exemption",
+                  code: "4",
+                  display:
+                    "Declined: Exemption from COVID vaccination and testing",
                 },
-                "contained": [
-                    {
-                        "id": "p1",
-                        "resourceType": "Patient",
-                        "birthDate": "1960-01-01"
-                    }
-                ],
-                "authored": "2021-08-13T17:15:00+00:00",
-                "item": [
-                    {
-                        "linkId": "exemptionStatus",
-                        "answer": [
-                            {
-                                "valueCoding": {
-                                    "system": "https://fhir.nhs.uk/CodeSystem/covid-vaccination-medical-exemption",
-                                    "code": "4",
-                                    "display": "Declined: Exemption from COVID vaccination and testing"
-                                }
-                            }
-                        ]
-                    },
-                    {
-                        "linkId": "exemptionPeriodStart",
-                        "answer": [
-                            {
-                                "valueDateTime": "2021-08-13T17:15:00+00:00"
-                            }
-                        ]
-                    },
-                    {
-                        "linkId": "exemptionPeriodEnd",
-                        "answer": [
-                            {
-                                "valueDateTime": "2022-08-13T17:15:00+00:00"
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-    ]
+              },
+            ],
+          },
+          {
+            linkId: "exemptionPeriodStart",
+            answer: [
+              {
+                valueDateTime: "2021-08-13T17:15:00+00:00",
+              },
+            ],
+          },
+          {
+            linkId: "exemptionPeriodEnd",
+            answer: [
+              {
+                valueDateTime: "2022-08-13T17:15:00+00:00",
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
 });
