b73e77f92d449e4c775b879684bf5d1843f9eabe
dominicplatt2-nhs <dominic.platt2@nhs.net>
2022-06-16 16:36:04 +0100

    Fix e2e issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -189,7 +189,7 @@ async def get_token_nhs_login_token_exchange(test_app: ApigeeApiDeveloperApps,
         },
     )
     assert token_resp["status_code"] == 200, 'failed getting token'
-    assert list(token_resp["body"].keys()) == ["access_token", "expires_in", "token_type", "issued_token_type"]
+    assert set(token_resp["body"].keys()).issuperset({"access_token", "expires_in", "token_type", "issued_token_type"})
     return token_resp["body"]
 
 
